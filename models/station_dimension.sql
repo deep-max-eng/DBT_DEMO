848df43b309e03d1b000520052df8447f09d1609
Deepika P <pillarisettideep@gmail.com>
2026-04-23 01:59:26 +0000

    station_dimension created

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+with BIKE as 
+(
+select distinct 
+START_STATION_NAME as STATION_NAME,
+START_STATION_ID as STATION_ID,
+START_LAT,
+START_LNG
+from {{ source('demo', 'bike') }}
+where RIDE_ID !='ride_id'
+
+
+
+)
+
+select * from BIKE
